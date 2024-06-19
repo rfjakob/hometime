dd044f0b719f2c306683e8b8f54d0335f0bdbe29
Jakob Unterwurzacher <jakobunt@gmail.com>
2024-06-19 22:24:30 +0200

    config.py: use light blue

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ SCHEDULE = {                                    # This doesn't get used if IGNOR
 
 PIXELS = 144                                    # The number of pixels on the neopixel strip
 GPIOPIN = 15                                    # The pin that the signal wire of the LED strip is connected to
-BARCOL = [(0, 20, 0), (0, 0, 100)]             # Color in RGB from 0 to 255
+BARCOL = [(0, 10, 10), (0, 0, 100)]             # Color in RGB from 0 to 255
 EVENTCOL = [(255, 255, 255), (255, 255, 0)]     # list of tuples used as meeting colors, if you only use one: [(255, 255, 255)]
 DISPLAY_EVENTS = True                           # Shows events
 FLIP = False                                    # Set to True if you want to flip the bar
